81108c6595e0f084bf43500d70d5d5f04adcbd1c
rovenelabanga <rovenelabanga2001@gmail.com>
2024-10-24 15:46:39 +0300

    adds media querry for mobile devices

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const navbar = document.querySelector("#navbar");
+
+document.getElementById("close").addEventListener("click", () => {
+  navbar.classList.toggle("hidden");
+});
+
+document.getElementById("menu-toggle").addEventListener("click", () => {
+  navbar.classList.toggle("hidden");
+});
+document.addEventListener("DOMContentLoaded", () => {});
